5bb91d6d5c9a3d84944544bb70bbadcce858d058
Sofia Hjerpe <hjerpesofia@gmail.com>
2023-12-22 12:02:30 +0100

    initiate a vue project, add basic alignment and style for nav

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+import HeaderComponent from "./components/HeaderComponent.vue";
+import HelloWorld from "./components/HelloWorld.vue";
+</script>
+
+<template>
+  <div class="header">
+    <div>
+      <HeaderComponent> </HeaderComponent>
+    </div>
+    <div>
+      <nav>
+        <RouterLink to="/">Planner</RouterLink>
+        <RouterLink to="/">Historik</RouterLink>
+        <RouterLink to="/">Tips</RouterLink>
+        <RouterLink to="/">Logga in</RouterLink>
+      </nav>
+    </div>
+  </div>
+
+  <RouterView />
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  flex-direction: column;
+  width: 1200px;
+  align-items: center;
+  justify-content: center;
+}
+
+nav {
+  width: 1001px;
+  top: 0;
+  display: flex;
+  justify-content: center;
+  gap: 150px;
+  align-items: center;
+  background-color: #fff;
+  box-shadow: 0px 4px 4px 0px #03382f1c;
+  height: 60px;
+}
+nav a {
+  color: #055647;
+  font-weight: 500;
+  text-transform: uppercase;
+  font-family: "Maven Pro", sans-serif;
+  font-size: 20px;
+}
+a:hover {
+  background-color: transparent;
+}
+</style>
---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+<template>
+  <div class="header"></div>
+</template>
+<style scoped>
+.header {
+  top: 0;
+  width: 1001px;
+  height: 540px;
+ 
+}
+</style>
